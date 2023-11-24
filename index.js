8fdc653ad0319ef976c45b5d212e1b74334c5b84
Grisza1976 <144440314+Grisza1976@users.noreply.github.com>
2023-11-24 11:27:07 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const mysql = require('mysql2');
+const app = express();
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'test'
+});
+
+function dodaj(dane, tab) {
+    let query = `INSERT INTO ${tab} SET ?`;
+  
+    connection.query(query, dane, function(err, results) {
+      if (err) {
+        console.error(err)
+        console.log(results);
+        return;
+      }
+    });
+  }
+
+  function aktualizuj(dane, warunek, tab) {
+    let query = `UPDATE ${tab} SET ?`;
+  
+    if (warunek) {
+      query += ` WHERE ${warunek}`;
+    }
+  
+    connection.query(query, dane, function(err, results) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    });
+  }
+
+function usun(parametr, tab) {
+    let query = `DELETE FROM ${tab}`;  
+    if (parametr) {
+      query += ` ${parametr}`;
+    }
+    connection.query(query, function(err, results) {
+      if (err) {
+        console.error(err);
+        console.log(results)
+        return;
+      }
+    });
+  }
+
+
+function pobierz(parametr, tab) {
+    let query = `SELECT * FROM ${tab}`;
+  
+    if (parametr) {
+      query += ` ${parametr}`;
+    }
+  
+    connection.query(query, function(err, results, fields) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(results);
+      console.log(fields);
+    });
+  }
+
+app.get("/", (req, res) => {
+  res.send(data());
+});
+
+app.listen(8080);
